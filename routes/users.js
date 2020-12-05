46402d953c263d551ae2ded1ecbb3ad8824f27d8
CloakingOcean <shawn.w8465@gmail.com>
2020-12-04 18:51:51 -0700

    Cathcing Up

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
-var express = require('express');
+var express = require("express");
+const jwt = require("jsonwebtoken");
 var router = express.Router();
 
+const JWT_KEY = process.env.JWT_KEY;
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
 
 module.exports = router;
