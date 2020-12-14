2bb0f64e4cf8e58684b6e6a9b9817fc0bf63aa54
Shawn Whitaker <shawn.w8465@gmail.com>
2020-12-14 16:59:42 -0700

    Merge feature-branch 'makes_email_unique' into master (#20)

--- data/schemas/users.schema.js
+++ data/schemas/users.schema.js
@@ -5,6 +5,7 @@ const SALT_WORK_FACTOR = 10;
 
 const UserSchema = new Schema({
   username: { type: String, required: true, index: { unique: true } },
+  email: { type: String, required: true, index: { unique: true } },
   password: { type: String, required: true },
   role: { type: String, required: true },
 });
