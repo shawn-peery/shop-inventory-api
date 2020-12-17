614fec10e5b25a697066c232ef2c27dde4642abe
Shawn Whitaker <shawn.w8465@gmail.com>
2020-12-16 20:38:22 -0700

    Merge feature-branch 'admin_see_all' into master (#23)

--- routes/users/users.js
+++ routes/users/users.js
@@ -10,11 +10,30 @@ const User = require("../../data/schemas/users.schema");
 router.get("/:id", async (req, res, next) => {
   const id = req.params.id;
 
-  const user = await User.findById({ _id: id })
+  var decoded = jwt.decode(res.locals.token);
+
+  const user = await User.findById({ _id: decoded._id });
+
+  console.log("decoded._id !== id");
+  console.log(decoded._id !== id);
+
+  console.log("user.role === admin");
+  console.log(user.role === "admin");
+
+  console.log("user");
+  console.log(user);
+
+  if (decoded._id !== id && user.role !== "admin") {
+    res
+      .status(400)
+      .json({ message: "You do not have access to this profile!" });
+    return;
+  }
+  const userObj = await User.findById({ _id: id })
     .select("email")
     .select("username");
 
-  res.status(200).json(user);
+  res.status(200).json(userObj);
 });
 
 module.exports = router;
